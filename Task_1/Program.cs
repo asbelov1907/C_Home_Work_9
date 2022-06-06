99dfc24b8489f8c76a553650d31d5e171c8819ac
ASBELOV <asbelov1907@gmail.com>
2022-06-06 23:09:00 +0300

    Задание 1, 2 выполнены

---
+++ Task_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+void SummNumber(int M, int N)
+{
+    int summ = 0;
+    for (int i = M; i <= N; i++)
+    {
+        summ = summ + i;
+        
+    }
+    Console.WriteLine("Сумма элементов в промежутке от M до N равна " + summ);
+}
+
+Console.WriteLine("Введите число M");
+int M = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine());
+SummNumber(M,N);
---
+++ Task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 29
+
+
+int Akkerman(int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+    else if (n > 0 && m == 0)
+        return Akkerman(n - 1, 1);
+    else if (n > 0 && m > 0)
+        return Akkerman(n - 1, Akkerman(n, m - 1));
+    else return 0;
+}
+
+Console.WriteLine("Введите число m");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число n");
+int n = int.Parse(Console.ReadLine());
+    int result = Akkerman(n, m); 
+    Console.WriteLine(result);
